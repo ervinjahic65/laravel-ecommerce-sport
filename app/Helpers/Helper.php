f092949ca6677b7b41e7455a371c1d2ddc2da6ad
ervinjahic65 <ervinjahic65@gmail.com>
2023-06-25 19:30:21 +0200

    knfdjsfnsjsd

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -6,12 +6,12 @@ use DB;
 class Helper
 {
 
-    public static function check_wishlist($id)
+    /* public static function check_wishlist($id)
     {
         $userid = session('userid');
         $result = DB::table('wishlist')->where('user_id',$userid)->where('product_id',$id)->first();
         return $result;
-    }
+    } */
 
 
 
